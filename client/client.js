fc6ed38248c0d75b17ec222ce40a82ba17565e92
Peter-Jan Celis <pj@celis.org>
2013-12-09 00:42:31 +0700

    Initial commit.

---
+++ client/client.js
@@ -0,0 +1,25 @@
+Template.messages.messages = function () {
+  return Messages.find({}, {sort: {time: 1}});
+}
+
+Template.input.events({
+  'keydown input#message': function (event) {
+    if (event.which == 13) {
+      var name = Meteor.user() ?
+      Meteor.user().profile.name :
+      "Anonymous";
+      var message = document.getElementById('message');
+
+      if (message.value != '') {
+        Messages.insert({
+          name: name,
+          message: message.value,
+          time: Date.now(),
+        });
+
+        document.getElementById('message').value = '';
+        message.value = '';
+      }
+    }
+  }
+});
